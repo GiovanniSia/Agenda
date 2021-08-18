00b32146d5676ea23d02dfe835d12126e02c2e22
leonelSubelza <subelza150@gmail.com>
2021-08-18 20:09:55 -0300

    Se suben cambios sobre nuevos datos para persona
    Se agrega la clase domicilio que tiene los nuevos datos de la persona.
    Se modifica el scriptAgenda con los nuevos campos para la persona.
    Y se agrega una librería para poner la fecha de cumpleaños.

--- agenda/sql/scriptAgenda.sql
+++ agenda/sql/scriptAgenda.sql
@@ -1,9 +1,15 @@
+DROP DATABASE IF EXISTS agenda;
 CREATE DATABASE `agenda`;
 USE agenda;
-CREATE TABLE `personas`
-(
-  `idPersona` int(11) NOT NULL AUTO_INCREMENT,
-  `Nombre` varchar(45) NOT NULL,
-  `Telefono` varchar(20) NOT NULL,
-  PRIMARY KEY (`idPersona`)
-);
+CREATE TABLE `personas`	(
+	`idPersona` int(11) NOT NULL AUTO_INCREMENT,
+  	`Nombre` varchar(45) NOT NULL,
+  	`Telefono` varchar(20) NOT NULL,
+	`calle` varchar(45) NOT NULL,
+	`altura` varchar(45) NOT NULL,
+	`piso` varchar(45) NOT NULL,
+	`departamento` varchar(45) NOT NULL,
+	`email` varchar(45) NOT NULL,
+	`fechaCumpleanios` Date,
+	PRIMARY KEY (`idPersona`)	
+	);
