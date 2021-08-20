a51815b038faf60f0f1aeb759798ed21189936dc
GiovanniSia <76817363+GiovanniSia@users.noreply.github.com>
2021-08-19 21:46:49 -0300

    Agrega tabla de tipo de contacto
    Se agrega la tabla y sus tres valores predeterminados.

--- agenda/sql/scriptAgenda.sql
+++ agenda/sql/scriptAgenda.sql
@@ -1,9 +1,24 @@
-CREATE DATABASE `agenda`;
+CREATE DATABASE IF NOT EXISTS `agenda`;
+
 USE agenda;
-CREATE TABLE `personas`
+
+CREATE TABLE IF NOT EXISTS `personas`
 (
   `idPersona` int(11) NOT NULL AUTO_INCREMENT,
   `Nombre` varchar(45) NOT NULL,
   `Telefono` varchar(20) NOT NULL,
   PRIMARY KEY (`idPersona`)
 );
+
+CREATE TABLE IF NOT EXISTS `tiposDeContactos`
+(
+    `idTipoContacto` int(11) NOT NULL AUTO_INCREMENT,
+    `tipoContacto` varchar(45) DEFAULT NULL,
+	PRIMARY KEY (`idTipoContacto`)
+);
+
+//Ingreso valores predeterminados a la tabla tiposDeContactos
+
+insert into tiposdecontactos values(1,"Trabajo");
+insert into tiposdecontactos values(2,"Familia");
+insert into tiposdecontactos values(3,"Amigos");
