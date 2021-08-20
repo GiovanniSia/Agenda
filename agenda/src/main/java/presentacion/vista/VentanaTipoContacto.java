59572ae43001e9cf639debbbd41cb6f170aba916
GiovanniSia <76817363+GiovanniSia@users.noreply.github.com>
2021-08-20 09:19:22 -0300

    Modifica VentanaTipoContacto
    Se agrega la funcionalidad del la ventana tanto por la "x" de cierre como del botón "salir"

--- agenda/src/main/java/presentacion/vista/VentanaTipoContacto.java
+++ agenda/src/main/java/presentacion/vista/VentanaTipoContacto.java
@@ -24,9 +24,10 @@ import java.util.List;
 import javax.swing.JComboBox;
 import javax.swing.JScrollPane;
 import javax.swing.JTable;
+import java.awt.event.ActionListener;
+import java.awt.event.ActionEvent;
 
-public class VentanaTipoContacto extends JFrame 
-{
+public class VentanaTipoContacto extends JFrame {
 	/**
 	 * 
 	 */
@@ -37,139 +38,123 @@ public class VentanaTipoContacto extends JFrame
 	private JButton btnBorrar;
 	private JButton btnSalir;
 	private DefaultTableModel modelTipoContacto;
-	private  String[] nombreColumnas = {"Id","Nombre"};
+	private String[] nombreColumnas = { "Id", "Nombre" };
 	private JLabel lblNewLabel;
 	private JTextField txtTipoContacto;
 
-	public VentanaTipoContacto() 
-	{
+	public VentanaTipoContacto() {
 		super();
 		initialize();
 	}
 
-
-	private void initialize() 
-	{
+	private void initialize() {
 		frame = new JFrame();
 		frame.setBounds(100, 100, 532, 271);
 		frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 		frame.getContentPane().setLayout(null);
-		
+
 		JPanel panel = new JPanel();
 		panel.setBounds(252, 0, 271, 232);
 		frame.getContentPane().add(panel);
 		panel.setLayout(null);
-		
+
 		JScrollPane spTipoContacto = new JScrollPane();
 		spTipoContacto.setBounds(10, 11, 250, 182);
 		panel.add(spTipoContacto);
-		
-		modelTipoContacto = new DefaultTableModel(null,nombreColumnas);
+
+		modelTipoContacto = new DefaultTableModel(null, nombreColumnas);
 		tablaTipoContacto = new JTable(modelTipoContacto);
-		
+
 		tablaTipoContacto.getColumnModel().getColumn(0).setPreferredWidth(103);
 		tablaTipoContacto.getColumnModel().getColumn(0).setResizable(false);
 		tablaTipoContacto.getColumnModel().getColumn(1).setPreferredWidth(100);
 		tablaTipoContacto.getColumnModel().getColumn(1).setResizable(false);
-		
+
 		spTipoContacto.setViewportView(tablaTipoContacto);
-		
+
 		btnSalir = new JButton("Salir");
+		btnSalir.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent e) {
+				System.exit(0);
+			}
+		});
 		btnSalir.setBounds(89, 198, 89, 23);
 		panel.add(btnSalir);
-		
+
 		JPanel panel_1 = new JPanel();
 		panel_1.setBounds(0, 0, 254, 232);
 		frame.getContentPane().add(panel_1);
 		panel_1.setLayout(null);
-		
+
 		btnAgregar = new JButton("Agregar");
 		btnAgregar.setBounds(10, 198, 71, 23);
 		panel_1.add(btnAgregar);
-		
+
 		JButton btnEditar = new JButton("Editar");
 		btnEditar.setBounds(91, 198, 71, 23);
 		panel_1.add(btnEditar);
-		
+
 		btnBorrar = new JButton("Borrar");
 		btnBorrar.setBounds(172, 198, 71, 23);
 		panel_1.add(btnBorrar);
-		
+
 		lblNewLabel = new JLabel("Modificar tipo de contacto");
 		lblNewLabel.setBounds(34, 11, 187, 14);
 		panel_1.add(lblNewLabel);
 		lblNewLabel.setFont(new Font("Tahoma", Font.PLAIN, 16));
-		
+
 		txtTipoContacto = new JTextField();
 		txtTipoContacto.setBounds(66, 50, 118, 20);
 		panel_1.add(txtTipoContacto);
 		txtTipoContacto.setColumns(10);
 	}
-	
-	public void show()
-	{
+
+	public void show() {
 		this.frame.setDefaultCloseOperation(JFrame.DO_NOTHING_ON_CLOSE);
-		this.frame.addWindowListener(new WindowAdapter() 
-		{
+		this.frame.addWindowListener(new WindowAdapter() {
 			@Override
-		    public void windowClosing(WindowEvent e) {
-		        int confirm = JOptionPane.showOptionDialog(
-		             null, "¿Estás seguro que quieres salir de la Agenda?", 
-		             "Confirmación", JOptionPane.YES_NO_OPTION,
-		             JOptionPane.QUESTION_MESSAGE, null, null, null);
-		        if (confirm == 0) {
-		        	Conexion.getConexion().cerrarConexion();
-		           System.exit(0);
-		        }
-		    }
+			public void windowClosing(WindowEvent e) {
+				System.exit(0);
+			}
 		});
 		this.frame.setVisible(true);
 	}
-	
-	public JButton getBtnAgregar() 
-	{
+
+	public JButton getBtnAgregar() {
 		return btnAgregar;
 	}
 
-	public JButton getBtnBorrar() 
-	{
+	public JButton getBtnBorrar() {
 		return btnBorrar;
 	}
-	
-	public JButton getBtnSalir() 
-	{
+
+	public JButton getBtnSalir() {
 		return btnSalir;
 	}
-	
-	public DefaultTableModel getModelTipoContacto() 
-	{
+
+	public DefaultTableModel getModelTipoContacto() {
 		return modelTipoContacto;
 	}
-	
-	public JTable getTablaTipoContacto()
-	{
+
+	public JTable getTablaTipoContacto() {
 		return tablaTipoContacto;
 	}
 
-	public String[] getNombreColumnas() 
-	{
+	public String[] getNombreColumnas() {
 		return nombreColumnas;
 	}
 
-
 	public void llenarTabla(List<TipoContactoDTO> tipoContactoEnTabla) {
-		this.getModelTipoContacto().setRowCount(0); //Para vaciar la tabla
+		this.getModelTipoContacto().setRowCount(0); // Para vaciar la tabla
 		this.getModelTipoContacto().setColumnCount(0);
 		this.getModelTipoContacto().setColumnIdentifiers(this.getNombreColumnas());
 
-		for (TipoContactoDTO t : tipoContactoEnTabla)
-		{
+		for (TipoContactoDTO t : tipoContactoEnTabla) {
 			int id = t.getIdTipoContacto();
 			String nombre = t.getNombreTipoContacto();
-			Object[] fila = {id, nombre};
+			Object[] fila = { id, nombre };
 			this.getModelTipoContacto().addRow(fila);
 		}
-		
+
 	}
 }
-
